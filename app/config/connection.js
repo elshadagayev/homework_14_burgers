f459c32ecfd6e160d4fb61f3e72b223dfc6c9fe7
elshadaghayev <elshad@cryptomessage.io>
2018-03-06 00:43:28 +0400

    commit

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b146266250c251",
+  password: "22cd5e65",
+  database: "heroku_9f87f8c18c30fd2"
 });
 
 connection.connect(function(err) {
@@ -13,6 +13,15 @@ connection.connect(function(err) {
     return;
   }
   console.log("connected as id " + connection.threadId);
+  connection.query(`CREATE TABLE if not exists burgers (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(120) NOT NULL,
+  devoured tinyint(3) unsigned NOT NULL DEFAULT '0',
+	  PRIMARY KEY (id)
+	) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1
+  `, function(err, data) {
+  	console.log(err, data);
+  });
 });
 
 module.exports = connection;
